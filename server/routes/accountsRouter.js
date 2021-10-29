2b137df3c553ac3d77873402a1f66a94bfd990a1
CaramelLatte <69405080+CaramelLatte@users.noreply.github.com>
2021-10-28 22:04:58 -0700

    Created account login/registry endpoint

--- server/routes/accountsRouter.js
+++ server/routes/accountsRouter.js
@@ -3,30 +3,53 @@ const accountsRouter = express.Router();
 //const fetch = require("node-fetch");
 
 const bcrypt = require("bcryptjs");
-const pass = "jared";
+const { Pool, Client } = require("pg");
+const { response } = require("express");
 
-// let check = bcrypt.compareSync(pass, hash);
-// if (check === true) {
-//   console.log("yes");
-// } else {
-//   console.log("no");
-// }
-
-accountsRouter.route("/").post((req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  console.log(`Username: ${username} password: ${password}`);
-
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
-
-  console.log(
-    `The username is ${username}, and the hashed password is ${hash}`
-  );
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.json(req.body);
+const pool = new Pool({
+  user: "nutdvvdgsxgsvk",
+  host: "ec2-18-234-15-247.compute-1.amazonaws.com",
+  database: "d1e9hugahp0n5r",
+  password: "7dc25c1edf0bd77382375842b0eae11de5d83303f61323a523169cc9e93f9b7b",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+accountsRouter
+  .route("/")
+  .get((req, res) => {
+    pool.query("SELECT * FROM users", (err, results) => {
+      if (err) console.log(err);
+      console.log(results.rows);
+      if (req.body.username === results.rows[0].user_username) {
+        res.status(200).json("Account match!");
+      } else {
+        res.status(200).json("Account not found");
+      }
+      let check = bcrypt.compareSync(
+        req.body.password,
+        results.rows[0].user_password
+      );
+      if (check === true) {
+        console.log("yes");
+      } else {
+        console.log("no");
+      }
+    });
+  })
+  .post((req, res) => {
+    let password = req.body.password;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    pool.query(
+      `INSERT INTO users (user_username, user_password) VALUES ('${req.body.username}', '${hash}')`
+    );
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.json(req.body);
+  });
 
 module.exports = accountsRouter;
